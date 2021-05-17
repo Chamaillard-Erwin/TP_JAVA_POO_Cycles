83967e775f9c7fb0f5bbf98d9511f36a2a151986
echamaillard2021 <erwin.chamaillard@laposte.net>
2021-05-17 16:51:59 +0200

    Réduction du code dans le main

--- src/fr/eni/main/Main.java
+++ src/fr/eni/main/Main.java
@@ -19,25 +19,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cycle[] tabCycle = new Cycle[TAILLE];
+        /**
+         * Tableau de Cycle que l'on initialise
+         */
+        Cycle[] tabCycle = {
+                new Velo("Lapierre speed 400", LocalDate.of(2019, 04, 15), 27, 4.90f),
+                new Velo("Btwin riverside 900", LocalDate.of(2015, 04, 15), 10, 4.90f),
+                new Gyropode("Segway Ninebot Elite", LocalDate.of(2021, 04, 15), 40, 1.50f, 29.90f),
+                new Gyropode("Weebot Echo", LocalDate.of(2018, 04, 15), 35, 1.60f, 29.90f),
+                new Gyroroue("Immotion v8", LocalDate.of(2020, 04, 15), 40, 18.90f),
+                new Gyroroue("Segway Nineboot One E+", LocalDate.of(2020, 04, 15), 30, 18.90f),
+        };
 
-        Cycle laPierre = new Velo("Lapierre speed 400", LocalDate.of(2019, 04, 15), 27, 4.90f);
-        Cycle btwin = new Velo("Btwin riverside 900", LocalDate.of(2015, 04, 15), 10, 4.90f);
-
-        Cycle segway = new Gyropode("Segway Ninebot Elite", LocalDate.of(2021, 04, 15), 40, 1.50f, 29.90f);
-        Cycle weebot = new Gyropode("Weebot Echo", LocalDate.of(2018, 04, 15), 35, 1.60f, 29.90f);
-
-        Cycle immotion = new Gyroroue("Immotion v8", LocalDate.of(2020, 04, 15), 40, 18.90f);
-        Cycle segway02 = new Gyroroue("Segway Nineboot One E+", LocalDate.of(2020, 04, 15), 30, 18.90f);
-
-       tabCycle[0] = laPierre;
-        tabCycle[1] = btwin;
-        tabCycle[2] = segway;
-        tabCycle[3] = weebot;
-        tabCycle[4] = immotion;
-        tabCycle[5] = segway02;
-
-
+        /**
+         * Lecture du tableau et on l'affiche
+         */
         for (Cycle cycle : tabCycle) {
             if (cycle != null) {
                 System.out.println(cycle.toString());
